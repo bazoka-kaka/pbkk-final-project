131414d33213c39c729e778c12a19ea8012240a2
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-10-15 20:42:55 +0700

    feat: add login page

--- routes/web.php
+++ routes/web.php
@@ -16,29 +16,11 @@ Route::post('/users', [UserController::class, 'store']);
 // Show Register/Create Form
 Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
-// Show Create Form
-// Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 
-// // Store Listing Data
-// Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
 
-// // Show Edit Form
-// Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->middleware('auth');
-
-// // Update Listing
-// Route::put('/jobs/{job}', [JobController::class, 'update'])->middleware('auth');
-
-// // Delete Listing
-// Route::delete('/jobs/{job}', [JobController::class, 'destroy'])->middleware('auth');
-
-// // Manage Listings
-// Route::get('/jobs/manage', [JobController::class, 'manage'])->middleware('auth');
-
-// // Log User Out
-// Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
-
-// // Show Login Form
-// Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
-
-// // Log In User
-// Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
