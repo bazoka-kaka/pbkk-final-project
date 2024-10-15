9765ec76d4d7b6b67d8b4dcccacfaabc930dafba
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-10-15 19:04:03 +0700

    feat: add home route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// All Listings
+Route::get('/', [JobController::class, 'index']);
