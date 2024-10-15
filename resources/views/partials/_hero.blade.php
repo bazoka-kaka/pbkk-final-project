3c93f626748c2891b64f9796b2355c1a8b462754
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-10-15 19:02:39 +0700

    feat: add hero component

---
+++ resources/views/partials/_hero.blade.php
@@ -0,0 +1,28 @@
+<section class="relative h-72 flex flex-col justify-center align-center text-center space-y-4 mb-4">
+  <div class="absolute top-0 left-0 w-full h-full opacity-10 bg-no-repeat bg-center"
+    style="background-image: url('images/dptsi.png')"></div>
+
+  <div class="z-10">
+    <h1 class="text-6xl font-bold uppercase text-blue-600">
+      DPTSI <span class="text-black">IT Jobs</span>
+    </h1>
+    <p class="text-2xl text-gray-500 my-4">
+      A list of IT job positions in DPTSI, ITS
+    </p>
+  </div>
+
+  <form action="/">
+    <div class="relative w-96 mx-auto border-2 border-gray-100 rounded-lg">
+      <div class="absolute top-4 left-3">
+        <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+      </div>
+      <input type="text" name="search" class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+        placeholder="Search for IT Jobs..." />
+      <div class="absolute top-2 right-2">
+        <button type="submit" class="h-10 w-20 text-white rounded-lg bg-blue-600 hover:bg-blue-500 transition duration-200">
+          Search
+        </button>
+      </div>
+    </div>
+  </form>
+</section>
