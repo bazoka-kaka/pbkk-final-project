9a5a4cd4e692512e769fc3ebb2260a23b737a6dc
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-10-15 19:00:33 +0700

    feat: update job controller

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class JobController extends Controller
 {
-    //
+    // Show all jobs
+    public function index() {
+        return view('jobs.index', [
+            'jobs' => Job::latest()->filter(request(['tag', 'search']))->paginate(6)
+        ]);
+    }
 }
