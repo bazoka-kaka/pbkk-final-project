cd7da85efc8c937644617748ff73e0c12236e121
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-10-15 19:01:21 +0700

    feat: add job factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Job;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'Yehezkiel Wiradhika',
+            'email' => 'yehezkiel1086@gmail.com'
+        ]);
+
+        Job::factory(6)->create([
+            'user_id' => $user->id
         ]);
     }
 }
