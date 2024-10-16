8ce0ad2cbe4b40400d8b4fa17bad9eafe4111447
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-10-16 22:19:59 +0700

    style: update manage styling

--- resources/views/jobs/manage.blade.php
+++ resources/views/jobs/manage.blade.php
@@ -1,25 +1,25 @@
 <x-layout>
-  <x-card class="p-10">
+  <div class="p-10">
     <header>
-      <h1 class="text-3xl text-center font-bold my-6 uppercase">
+      <h1 class="text-3xl text-center font-bold mt-6 uppercase">
         Manage Jobs
       </h1>
     </header>
 
-    <table class="w-full table-auto rounded-sm">
+    <table class="w-full table-auto rounded-sm mt-16">
       <tbody>
         @unless($jobs->isEmpty())
         @foreach($jobs as $job)
         <tr class="border-gray-300">
-          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+          <td class="px-4 py-8 border-b border-gray-300 text-lg">
             <a href="/jobs/{{$job->id}}"> {{$job->title}} </a>
           </td>
-          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+          <td class="px-4 py-8 border-b border-gray-300 text-lg">
             <a href="/jobs/{{$job->id}}/edit" class="text-blue-400 px-6 py-2 rounded-xl"><i
                 class="fa-solid fa-pen-to-square"></i>
               Edit</a>
           </td>
-          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+          <td class="px-4 py-8 border-b border-gray-300 text-lg">
             <form method="POST" action="/jobs/{{$job->id}}">
               @csrf
               @method('DELETE')
@@ -30,7 +30,7 @@
         @endforeach
         @else
         <tr class="border-gray-300">
-          <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+          <td class="px-4 py-8 border-b border-gray-300 text-lg">
             <p class="text-center">No Listings Found</p>
           </td>
         </tr>
@@ -38,5 +38,5 @@
 
       </tbody>
     </table>
-  </x-card>
+  </div>
 </x-layout>
